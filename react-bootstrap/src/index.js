906593d30118fafd97bff084dcd14dc06fad5ed7
Geonhwi Park <geonhwipark715@gmail.com>
2024-08-21 16:34:27 +0900

    가위바위보 게임

--- react-bootstrap/src/index.js
+++ react-bootstrap/src/index.js
@@ -10,11 +10,12 @@ import AutoLayoutSizingExample from './bootstrap/AutoLayoutSizingExample';
 import VerticalExample from './bootstrap/VerticalExample';
 import HorizontalExample from './bootstrap/HorizontalExample';
 import BasicExample from './bootstrap/BasicExample';
+import Game from './bootstrap/Game';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BasicExample />
+    <Game />
   </React.StrictMode>
 );
 
